7b928c9f58889033b57aa7d3a3fa8dba209543db
papicool <papemfall@gmail.com>
2023-07-03 16:35:42 +0000

    Week-X CICD Pipeline and Create Activity

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -10,7 +10,12 @@ VALUES
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'papamfall' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'altbrown' LIMIT 1),
+    'I am the other!',
+    current_timestamp + interval '10 day'
+  );
